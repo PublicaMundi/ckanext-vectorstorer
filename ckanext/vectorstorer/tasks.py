2ba2e92aaf5dc51b4f8ef49d1385b166d009cdb7
Alex Theleritis <atheleritis@getmap.gr>
2014-07-11 13:07:50 +0300

    pass more metadata to geoserver layers
    pass more metadata to geoserver layers like layer title and description

--- ckanext/vectorstorer/tasks.py
+++ ckanext/vectorstorer/tasks.py
@@ -101,9 +101,9 @@ def _handle_vector(gdal_driver,resource_tmp_folder,resource,db_conn_params,conte
 	vector_layer=vector.Vector()
 	vector_layer.open_file(gdal_driver,file_path,resource['id'],db_conn_params)
     
-    wms_server,wms_layer=_publish_layer(geoserver_context,resource['id'])
+    wms_server,wms_layer=_publish_layer(geoserver_context, resource)
     _update_resource_metadata(context,resource)
-    _add_wms_resource(context,resource['id'],resource['name'],wms_server,wms_layer)
+    _add_wms_resource(context, resource, wms_server, wms_layer)
 
 
 def _delete_temp(res_tmp_folder):
@@ -130,22 +130,27 @@ def _is_shapefile(res_folder_path):
      
       
          
-def _publish_layer(geoserver_context,table_name):
+def _publish_layer(geoserver_context,resource):
     geoserver_url= geoserver_context['geoserver_url']
     geoserver_workspace= geoserver_context['geoserver_workspace']
     geoserver_admin= geoserver_context['geoserver_admin']
     geoserver_password= geoserver_context['geoserver_password']
     geoserver_ckan_datastore= geoserver_context['geoserver_ckan_datastore']
     
+    resource_id=resource['id'].lower()
+    resource_name=resource['name'].split('.')[0]
+    resource_description=resource['description']
+    
     url=geoserver_url+"/rest/workspaces/"+geoserver_workspace+"/datastores/"+geoserver_ckan_datastore+"/featuretypes"
     req = urllib2.Request(url)
     req.add_header("Content-type", "text/xml")
-    req.add_data("<featureType><name>"+table_name.lower()+"</name></featureType>")
+    req.add_data("<featureType><name>%s</name><title>%s</title><abstract>%s</abstract></featureType>"%(resource_id,resource_name,resource_description))
     req.add_header('Authorization',"Basic " + (geoserver_admin+':'+geoserver_password).encode("base64").rstrip())
  
     res = urllib2.urlopen(req)
     wms_server=geoserver_url+"/wms"
-    wms_layer=geoserver_workspace+":"+table_name.lower()
+    wms_layer=geoserver_workspace+":"+resource_id
+    
     return wms_server,wms_layer
 
 def _update_resource_metadata(context,resource):
@@ -157,7 +162,7 @@ def _update_resource_metadata(context,resource):
     request.add_header('Authorization', api_key)
     urllib2.urlopen(request, data_string)
     
-def _add_wms_resource(context,resource_id,resource_name,wms_server,wms_layer):
+def _add_wms_resource(context, parent_resource, wms_server, wms_layer):
     
     api_key=context['apikey'].encode('utf8')
     site_url=context['site_url']
@@ -167,11 +172,13 @@ def _add_wms_resource(context,resource_id,resource_name,wms_server,wms_layer):
 	  "package_id":unicode(package_id),
 	  "url":wms_server+"?service=WMS&request=GetCapabilities",
 	  "format":u'WMS',
-	  "from_uuid":unicode(resource_id),
+	  "from_uuid":unicode(parent_resource['id']),
 	  'vectorstorer_resource':True,
 	  "wms_server":unicode(wms_server) ,
 	  "wms_layer":unicode(wms_layer),
-	  "name":resource_name.split('.')[0]+" WMS Layer",}
+	  "name":parent_resource['name'].split('.')[0]+" WMS Layer",
+	  "description":parent_resource['description']}
+    
     data_string = urllib.quote(json.dumps(resource))
     request = urllib2.Request(site_url+'api/action/resource_create')
     request.add_header('Authorization', api_key)
@@ -188,7 +195,6 @@ def vectorstorer_delete( geoserver_cont,cont,data):
     db_conn_params=context['db_params']
     if resource:
 	_delete_from_datastore(resource,db_conn_params,context)
-	_unpublish_from_geoserver(resource['id'],geoserver_context)
 	_delete_vectorstorer_resources(resource,context)
     else:
        	_unpublish_from_geoserver(context['vector_storer_resources_ids'][0],geoserver_context)
