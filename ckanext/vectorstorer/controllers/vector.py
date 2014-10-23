8b537a1e9abb64a4aef4eb1c886e1aeaaf5f6902
alextheleritis <atheleritis@getmap.gr>
2014-10-23 17:14:08 +0300

    Added extra parameters for csv, xls resources

--- ckanext/vectorstorer/controllers/vector.py
+++ ckanext/vectorstorer/controllers/vector.py
@@ -14,17 +14,25 @@ class VectorController(BaseController):
     '''VectorController will be used to publish vector data at postgis and geoserver'''
     
     def publish(self):
+
 	resource_id = request.params.get('resource_id',u'')
 	self._get_context(resource_id)
 	
         encoding = self._get_encoding()
         projection = self._get_projection()    
         
+        #Extra parm for csv,xls
+        geometry_column, geometry_type = self._get_geometry_params() 
+        
         resource=ckan.model.Session.query(model.Resource).get(resource_id)
-        extra_params={
+       
+	extra_params={
 		      "encoding":encoding,
-		      "projection":projection
+		      "projection":projection,
+		      "geometry_type":geometry_type,
+		      "geometry_column":geometry_column
 		      }
+	
         resource_actions.create_vector_storer_task(resource,extra_params)
         
     def _get_context(self,resource_id):
@@ -45,9 +53,11 @@ class VectorController(BaseController):
             
     def _get_encoding(self):
 	_encoding=request.params.get('encoding',u'utf-8')
+	
 	if len(_encoding)==0:
 	    _encoding=u'utf-8'
 	    return _encoding
+	
 	else:
 	    if self._encoding_exists(_encoding):
 		return _encoding
@@ -75,3 +85,19 @@ class VectorController(BaseController):
 	    abort(400, _('Bad EPSG code : %s') % proj_param)
 	except OverflowError:
 	    abort(400, _('Bad EPSG code : %s') % proj_param)
+
+    def _get_geometry_params (self):
+	
+	_geometry_column_param = request.params.get('geometry_column',u'')
+	_geometry_type_param = request.params.get('geometry_type',u'')
+	
+	_geometry_type = None
+	_geometry_column = None
+	
+	if _geometry_type_param.upper() in ['WKT','XY','YX','XYZ']:
+	    _geometry_type = _geometry_type_param
+	
+	if len(_geometry_column_param)==0:
+	    _geometry_column=None
+	    
+	return _geometry_column, _geometry_type
